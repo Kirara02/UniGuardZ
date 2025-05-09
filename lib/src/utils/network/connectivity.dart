4a4da0f6ff8bf8bb919468ace7813c3d6526b7a7
Kirara02 <fahtul.singaparna@gmail.com>
2025-05-09 08:23:31 +0700

    feat: edit connectivity check

--- lib/src/utils/network/connectivity.dart
+++ lib/src/utils/network/connectivity.dart
@@ -2,7 +2,8 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 
 class NetworkConnectivity {
   static Future<bool> isConnected() async {
-    final connectivityResult = await Connectivity().checkConnectivity();
-    return connectivityResult != ConnectivityResult.none;
+    final results = await Connectivity().checkConnectivity();
+    return results.contains(ConnectivityResult.mobile) ||
+        results.contains(ConnectivityResult.wifi);
   }
 }
